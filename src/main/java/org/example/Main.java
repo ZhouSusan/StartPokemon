abc5953fe09d2e9d68eb17e77636ebbde1549f24
ZhouSusan <szhou089@gmail.com>
2022-10-07 20:04:14 -0700

    created squritle object and tested it's method in main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        Squirtle squirtle = new Squirtle("Squirtle", "water", "female");
+        squirtle.speak();
+        squirtle.trainAbility();
+        squirtle.eat();
+        squirtle.whoAmI();
     }
 }
