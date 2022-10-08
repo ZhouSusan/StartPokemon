75710a85f54f18d31e533ac75c09dac66499aaeb
ZhouSusan <szhou089@gmail.com>
2022-10-08 16:57:31 -0700

    changed main.java file name to Pokemon_Center.java

--- src/main/java/org/example/Pokemon_Center.java
+++ src/main/java/org/example/Pokemon_Center.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Main {
+public class Pokemon_Center {
     public static void main(String[] args) {
         Squirtle squirtle = new Squirtle("Squirtle", "water", "female");
         squirtle.speak();
