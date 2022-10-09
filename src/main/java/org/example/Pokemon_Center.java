a08df0f4240db363bf100eed239a0e2c2e8b78bb
ZhouSusan <szhou089@gmail.com>
2022-10-08 17:03:55 -0700

    added items to ArrayList

--- src/main/java/org/example/Pokemon_Center.java
+++ src/main/java/org/example/Pokemon_Center.java
@@ -5,21 +5,13 @@ public class Pokemon_Center {
     public static void main(String[] args) {
         ArrayList <Pokemon> pokeyTeam = new ArrayList <>();
         Squirtle squirtle = new Squirtle("Squirtle", "water", "female");
-        squirtle.speak();
-        squirtle.trainAbility();
-        squirtle.eat();
-        squirtle.whoAmI();
-
         Charmander charmander = new Charmander("Charmander", "fire", "water");
-        charmander.speak();
-        charmander.trainAbility();
-        charmander.eat();
-        charmander.whoAmI();
-
         Bulbasaur bulbasaur = new Bulbasaur("Bulbasaur", "Grass", 19.8);
-        bulbasaur.speak();
-        bulbasaur.trainAbility();
-        bulbasaur.eat();
-        bulbasaur.whoAmI();
+
+        pokeyTeam.add(charmander);
+        pokeyTeam.add(squirtle);
+        pokeyTeam.add(bulbasaur);
+
+        
     }
 }
